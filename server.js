88bea0bfb4bf98553ba5cd6c12ecfd3360f46bc3
ekslffh <ekslffh@naver.com>
2021-12-16 17:05:01 +0900

    Create Data Table & Connect React Project

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,33 +7,23 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extends: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connection.connect();
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "나성민",
-      birthday: "990201",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "아이유",
-      birthday: "901208",
-      gender: "여자",
-      job: "가수",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "황정민",
-      birthday: "690219",
-      gender: "남자",
-      job: "배우",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, console.log(`Listening on port ${port}`));
